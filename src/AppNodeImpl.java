379f94acedc6009df09c23ef98638b70dfd27d64
MichaelDik <p3180050@aueb.gr>
2021-04-13 13:02:15 +0300

    Loaded Kostas functions

--- src/AppNodeImpl.java
+++ src/AppNodeImpl.java
@@ -1,3 +1,6 @@
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -55,7 +58,27 @@ public class AppNodeImpl implements Publisher, Consumer{
 
     @Override
     public ArrayList<Value> generateChunks(String chunk) {
-        return null;
+        ArrayList<byte[]> my_arraylist = new ArrayList<byte []>();
+
+        //to epomeno arraylist kai byte array einai mono gia thn arxikopoihsh tou VideoFile
+        ArrayList<String> empty = null;
+        byte[] arr = null;
+        VideoFile vf = new VideoFile("", "", "", "", "", "",
+                "", empty, arr);
+
+        boolean flag = true;
+        int i = 0;
+        byte[] inputBuffer = vf.getVideoFileChunk("C:\\Users\\Kostas\\IdeaProjects\\Distributed Systems\\src\\DIMAKHS.mp4");
+
+        while (i < inputBuffer.length) {
+            byte[] buffer = new byte[4096];
+            for (int j = 0;j < buffer.length;j++) {
+                if (i < inputBuffer.length)
+                    buffer[j] = inputBuffer[i++];
+            }
+            my_arraylist.add(buffer);
+        }
+        return my_arraylist;
     }
 
     @Override
@@ -81,5 +104,23 @@ public class AppNodeImpl implements Publisher, Consumer{
     @Override
     public void updateNodes() {
 
+    }
+
+    public static void main(String[] args) {
+        Publisher p = new AppNodeImpl();
+        ArrayList<byte[]> inputArraylist = p.generateChunks();
+        try {
+            File nf = new File("C:/Users/Kostas/Desktop/test.mp4");
+            for (byte[] ar : inputArraylist) {
+                FileOutputStream fw = new FileOutputStream(nf, true);
+                try {
+                    fw.write(ar);
+                } finally {
+                    fw.close();
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/VideoFile.java
+++ src/VideoFile.java
@@ -1,3 +1,6 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class VideoFile {
@@ -23,5 +26,19 @@ public class VideoFile {
         this.frameHeight = frameHeight;
         this.associatedHashtags = associatedHashtags;
         this.videoFileChunk = videoFileChunk;
+    }
+
+    public byte[] getVideoFileChunk(String filepath) {
+        try {
+            FileInputStream fin = new FileInputStream(new File(filepath));
+            byte[] buffer = new byte[(int)new File(filepath).length()];
+            fin.read(buffer);
+            fin.close();
+            return buffer;
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        return new byte[0];
     }
 }
