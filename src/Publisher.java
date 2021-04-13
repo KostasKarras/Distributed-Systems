a05cb66fd5e59bd9d6f2016cb43a125f3477ae4a
Konstantinos Karras <81237959+KostasKarras@users.noreply.github.com>
2021-04-13 03:35:19 +0300

    Update Publisher.java

--- src/Publisher.java
+++ src/Publisher.java
@@ -1,39 +1,83 @@
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Publisher extends Node{
+public class Publisher extends Node {
 
-    private static final ChannelName channelName = null;
+    public static final ChannelName channelName = null;
+    public static final List<Broker> brokers = null;
 
-    public void addHashTag(String hashtag){
-
-    }
-
-    public void removeHashTag(String hashtag){
+    public void addHashTag(String hashtag) {
 
     }
 
-    public List<Broker> getBrokerList(){
+    public void removeHashTag(String hashtag) {
 
     }
 
-    public Broker hashTopic(String hashtopic){
+    public List<Broker> getBrokerList() {
+        return super.getBrokers();
+    }
+
+//    public Broker hashTopic(String hashtopic){
+//
+//    }
+
+    public void push(String hashtags, Value video) {
 
     }
 
-    public void push(String hashtags, Value video){
+    public void notifyFailure(Broker broker) {
 
     }
 
-    public void notifyFailure(Broker broker){
+    public void notifyBrokersForHashTags(String hashtag) {
 
     }
 
-    public void notifyBrokersForHashTags(String hashtag){
+    public ArrayList<byte[]> generateChunks() {
+        ArrayList<byte[]> my_arraylist = new ArrayList<byte []>();
 
+        //to epomeno arraylist kai byte array einai mono gia thn arxikopoihsh tou VideoFile
+        ArrayList<String> empty = null;
+        byte[] arr = null;
+        VideoFile vf = new VideoFile("", "", "", "", "", "",
+                "", empty, arr);
+
+        boolean flag = true;
+        int i = 0;
+        byte[] inputBuffer = vf.getVideoFileChunk("C:\\Users\\Kostas\\IdeaProjects\\Distributed Systems\\src\\DIMAKHS.mp4");
+
+        while (i < inputBuffer.length) {
+            byte[] buffer = new byte[4096];
+            for (int j = 0;j < buffer.length;j++) {
+                if (i < inputBuffer.length)
+                    buffer[j] = inputBuffer[i++];
+            }
+            my_arraylist.add(buffer);
+        }
+        return my_arraylist;
     }
 
-    public ArrayList<Value> generateChunks(String chunk){
-
+    public static void main(String[] args) {
+        Publisher p = new Publisher();
+        ArrayList<byte[]> inputArraylist = p.generateChunks();
+        for(byte[] ar : inputArraylist)
+            System.out.println(ar);
+        try {
+            File nf = new File("C:/Users/Kostas/Desktop/test.mp4");
+            for (byte[] ar : inputArraylist) {
+                FileOutputStream fw = new FileOutputStream(nf, true);
+                try {
+                    fw.write(ar);
+                } finally {
+                    fw.close();
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
