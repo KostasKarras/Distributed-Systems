5874a7913c0251e17d6365873d22a0e4b01e6564
Konstantinos Karras <81237959+KostasKarras@users.noreply.github.com>
2021-04-13 03:36:24 +0300

    Update VideoFile.java

--- src/VideoFile.java
+++ src/VideoFile.java
@@ -1,3 +1,6 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class VideoFile {
@@ -10,10 +13,10 @@ public class VideoFile {
     private String frameWidth;
     private String frameHeight;
     private ArrayList<String> associatedHashtags;
-    private Byte[] videoFileChunk;
+    private byte[] videoFileChunk;
 
     public VideoFile(String videoName, String channelName, String dateCreated, String length, String frameRate, String frameWidth,
-                     String frameHeight, ArrayList<String> associatedHashtags, Byte[] videoFileChunk) {
+                     String frameHeight, ArrayList<String> associatedHashtags, byte[] videoFileChunk) {
         this.videoName = videoName;
         this.channelName = channelName;
         this.dateCreated = dateCreated;
@@ -23,5 +26,19 @@ public class VideoFile {
         this.frameHeight = frameHeight;
         this.associatedHashtags = associatedHashtags;
         this.videoFileChunk = videoFileChunk;
+    }
+
+    public byte[] getVideoFileChunk(String filepath) {
+        try {
+            FileInputStream fin = new FileInputStream(new File(filepath));
+            byte[] buffer = new byte[(int)new File(filepath).length()];
+            fin.read(buffer);
+            fin.close();
+            return buffer;
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        return new byte[0];
     }
 }
