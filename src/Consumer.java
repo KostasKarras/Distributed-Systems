7c6ce804ef25927d223f2b24cab24adc49206b7e
Konstantinos Karras <81237959+KostasKarras@users.noreply.github.com>
2021-04-15 14:59:10 +0300

    Delete Consumer.java

--- src/Consumer.java
+++
@@ -1,9 +0,0 @@
-interface Consumer extends Node{
-
-    public void register(Broker broker, String str);
-
-    public void disconnect(Broker broker, String str);
-
-    public void playData(String str, Value video);
-
-}
