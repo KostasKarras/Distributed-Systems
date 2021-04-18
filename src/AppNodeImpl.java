e42b2a5123437c8a3e77b2767832614db632b9c7
Konstantinos Karras <81237959+KostasKarras@users.noreply.github.com>
2021-04-18 14:53:27 +0300

    Update AppNodeImpl.java

--- src/AppNodeImpl.java
+++ src/AppNodeImpl.java
@@ -5,151 +5,21 @@ import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class AppNodeImpl extends Thread implements Publisher, Consumer{
-    @Override
-    public void push(String hashtags, Value video) {
-        this.start();
-        video.addAssociatedHashTags(hashtags);
-        this.addHashTag(hashtags);
-    }
+public class AppNodeImpl implements Publisher, Consumer{
 
-    Value value;
-    String type;
-    public AppNodeImpl(String type, Value value){
-        this.type = type;
-        this.value = value;
-    }
+    private static Socket requestSocket;
+    private static ObjectOutputStream objectOutputStream;
+    private static ObjectInputStream objectInputStream;
+    private static ChannelName channel;
 
-    public ArrayList<byte[]> generateChunks(String filepath, ArrayList<String> hashtags) {
-        ArrayList<byte[]> my_arraylist = new ArrayList<byte []>();
-
-        VideoFile vf = new VideoFile(filepath, hashtags);
-
-        boolean flag = true;
-        int i = 0;
-        byte[] inputBuffer = vf.getVideoFileChunk();
-
-        while (i < inputBuffer.length) {
-            byte[] buffer = new byte[4096];
-            for (int j = 0;j < buffer.length;j++) {
-                if (i < inputBuffer.length)
-                    buffer[j] = inputBuffer[i++];
-            }
-            my_arraylist.add(buffer);
-        }
-        return my_arraylist;
-    }
-
-    public void run(){
-        ArrayList<byte[]> chunks = generateChunks(this.getValue().getFilepath(), this.getValue().getAssociatedHashtags());
-        Socket requestSocket = null;
-        ObjectOutputStream objectOutputStream = null;
-        ObjectInputStream objectInputStream = null;
-        String message;
-
-/*
------------------------------------
-PORTS TO CONNECT THE REQUEST SOCKET
------------------------------------
-----------
-PORT: 7023
-PORT: 5789
-PORT: 4555
-----------
-*/
-
-        try {
-            requestSocket = new Socket(InetAddress.getByName("127.0.0.1"), 4555);
-
-            objectOutputStream = new ObjectOutputStream(requestSocket.getOutputStream());
-            objectInputStream = new ObjectInputStream(requestSocket.getInputStream());
-
-            try {
-                objectOutputStream.writeObject("Hello! New Client here!");
-                objectOutputStream.flush();
-
-                message = (String) objectInputStream.readObject();
-                System.out.println("Server>" + message);
-
-                objectOutputStream.writeObject(chunks.size());
-                objectOutputStream.flush();
-
-                while (!chunks.isEmpty()) {
-                    byte[] clientToServer = chunks.remove(0);
-                    objectOutputStream.write(clientToServer);
-                    objectOutputStream.flush();
-                }
-            } catch (UnknownHostException unknownHost) {
-                System.err.println("You are trying to connect to an unknown host!");
-            } catch (IOException ioException) {
-                ioException.printStackTrace();
-            } catch (ClassNotFoundException e) {
-                e.printStackTrace();
-            } finally {
-                try {
-                    objectOutputStream.writeObject("Bye");
-                    objectOutputStream.flush();
-                    objectInputStream.close();
-                    objectOutputStream.close();
-                    requestSocket.close();
-                } catch (IOException ioException) {
-                    ioException.printStackTrace();
-                }
-            }
-        } catch (UnknownHostException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static void main(String[] args) {
-//        //generateChunks argument is the filepath
-//        ArrayList<byte[]> inputArraylist = p.generateChunks("C:\\Users\\Kostas\\IdeaProjects\\Distributed Systems\\src\\DIMAKHS.mp4");
-//        try {
-//            File nf = new File("C:/Users/Kostas/Desktop/test.mp4");
-//            for (byte[] ar : inputArraylist) {
-//                FileOutputStream fw = new FileOutputStream(nf, true);
-//                try {
-//                    fw.write(ar);
-//                } finally {
-//                    fw.close();
-//                }
-//            }
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-        ArrayList<String> hashtags1 = new ArrayList<String>();
-        hashtags1.add("First File");
-        VideoFile vf = new VideoFile("C:\\Users\\Kostas\\Downloads\\yt1s.com - Dji Mavic mini  Video test cinematic_720p.mp4", hashtags1);
-        Value v = new Value(vf);
-        //p.push("#TIPOTES", v);
-        Publisher p = new AppNodeImpl("p", v);
-        p.push("#TIPOTES", v);
-
-        ArrayList<String> hashtags2 = new ArrayList<String>();
-        hashtags2.add("Second File");
-        VideoFile vf2 = new VideoFile("C:\\Users\\Kostas\\IdeaProjects\\Distributed Systems\\src\\DIMAKHS.mp4", hashtags2);
-        Value v2 = new Value(vf2);
-        //p2.push("#TIPOTES", v2);
-        Publisher p2 = new AppNodeImpl("p", v2);
-        p2.push("#TIPOTES", v2);
-    }
-
-    public Value getValue(){
-        return this.value;
-    }
-
-    //Hashtags for which the publisher is responsible
-    ArrayList<String> hashtags = new ArrayList<String>();
     @Override
     public void addHashTag(String hashtag) {
-        hashtags.add(hashtag);
+        channel.addHashTag(hashtag);
     }
 
     @Override
     public void removeHashTag(String hashtag) {
-        hashtags.remove(hashtag);
+        channel.removeHashTag(hashtag);
     }
 
     @Override
@@ -163,6 +33,55 @@ PORT: 4555
     }
 
     @Override
+    public void push(String hashtags, VideoFile video) {
+
+        ArrayList<byte[]> chunks = generateChunks(video);
+        String message;
+
+        connect();
+
+        try {
+
+            objectOutputStream.writeObject(1);
+            objectOutputStream.flush();
+
+            objectOutputStream.writeObject("I want to push a new video!");
+            objectOutputStream.flush();
+
+            message = (String) objectInputStream.readObject();
+            System.out.println("Server>" + message);
+
+            objectOutputStream.writeObject(chunks.size());
+            objectOutputStream.flush();
+
+            objectOutputStream.writeObject(hashtags);
+            objectOutputStream.flush();
+
+            while (!chunks.isEmpty()) {
+                byte[] clientToServer = chunks.remove(0);
+                objectOutputStream.write(clientToServer);
+                objectOutputStream.flush();
+            }
+        } catch (UnknownHostException unknownHost) {
+            System.err.println("You are trying to connect to an unknown host!");
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                objectOutputStream.writeObject("Bye");
+                objectOutputStream.flush();
+
+                disconnect();
+                addHashTag(hashtags);
+            } catch (IOException ioException) {
+                ioException.printStackTrace();
+            }
+        }
+    }
+
+    @Override
     public void notifyFailure(Broker broker) {
 
     }
@@ -173,28 +92,87 @@ PORT: 4555
     }
 
     @Override
-    public void init() {
+    public ArrayList<byte[]> generateChunks(VideoFile video) {
+        ArrayList<byte[]> my_arraylist = new ArrayList<byte []>();
+
+        boolean flag = true;
+        int i = 0;
+        byte[] inputBuffer = video.getVideoFileChunk();
+
+        while (i < inputBuffer.length) {
+            byte[] buffer = new byte[4096];
+            for (int j = 0;j < buffer.length;j++) {
+                if (i < inputBuffer.length)
+                    buffer[j] = inputBuffer[i++];
+            }
+            my_arraylist.add(buffer);
+        }
+        return my_arraylist;
+    }
+
+    @Override
+    public void initialize(int port) {
+
+        channel = new ChannelName("user");
+
+        ArrayList<String> videoHashtags = new ArrayList<>();
+        videoHashtags.add("First File");
+
+        VideoFile vf = new VideoFile("C:\\Users\\Kostas\\IdeaProjects\\Distributed Systems\\src\\DIMAKHS.mp4", videoHashtags);
+        push("#TIPOTES", vf);
 
     }
 
     @Override
     public List<Broker> getBrokers() {
-        return brokers;
-    }
-
-    @Override
-    public Socket connect() {
         return null;
     }
 
     @Override
-    public void disconnect() {
+    public void connect() {
+
+        try {
+            requestSocket = new Socket(InetAddress.getByName("127.0.0.1"), 4321);
+            objectOutputStream = new ObjectOutputStream(requestSocket.getOutputStream());
+            objectInputStream = new ObjectInputStream(requestSocket.getInputStream());
+        } catch (UnknownHostException unknownHost) {
+            System.err.println("You are trying to connect to an unknown host.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
+
+    public void disconnect() {
+        try {
+            objectInputStream.close();
+            objectOutputStream.close();
+            requestSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        finally {
+            System.out.println("Socket disconnected!");
+        }
+    };
 
     @Override
     public void updateNodes() {
 
+    }
+
+    /**KOSTAS*/
+    public static void main(String[] args) {
+        new AppNodeImpl().initialize(4321);
+
+        AppNodeImpl consumer = new AppNodeImpl();
+        ArrayList<String> associatedHashtags = new ArrayList<String>();
+        //I make a new empty constructor, because the is no reason to have the video file details
+        VideoFile vf = new VideoFile();
+        //If the topic is #TIPOTES then the playData method is working
+        //But if you change that (even a letter) then the file isn't pulled
+        //Because the publisher pushed the video with hashtag #TIPOTES
+        consumer.playData("#TIPOTES", vf);
     }
 
     @Override
@@ -207,8 +185,69 @@ PORT: 4555
 
     }
 
+    /**KOSTAS*/
     @Override
-    public void playData(String str, Value video) {
+    public void playData(String topic, VideoFile video) {
+        String message;
 
+        connect();
+
+        try {
+
+            //Just to wait the publisher push the video(It is for testing...)
+            Thread.sleep(10000);
+
+            //Handle pull code equals to 2
+            objectOutputStream.writeObject(2);
+            objectOutputStream.flush();
+
+            objectOutputStream.writeObject("I want to pull a video!");
+            objectOutputStream.flush();
+
+            byte[] chunk;
+            ArrayList<byte[]> chunks = new ArrayList<byte[]>();
+
+            int size = (int) objectInputStream.readObject();
+
+            //Sends topic to server
+            objectOutputStream.writeObject(topic);
+            objectOutputStream.flush();
+
+            message = (String) objectInputStream.readObject();
+            System.out.println("Server>" + message);
+
+            message = (String) objectInputStream.readObject();
+            System.out.println("Server>" + message);
+
+            for (int i = 0; i < size; i++) {
+                chunk = new byte[4096];
+                chunk = objectInputStream.readAllBytes();
+                chunks.add(chunk);
+            }
+
+            String videoExists = (String)objectInputStream.readObject();
+            //The new file, test, in Desktop created only if the video exists.
+            if(videoExists.equals("true")) {
+                try {
+                    File nf = new File("C:\\Users\\Kostas\\Desktop\\test.mp4");
+                    for (byte[] ar : chunks) {
+                        FileOutputStream fw = new FileOutputStream(nf, true);
+                        try {
+                            fw.write(ar);
+                        } finally {
+                            fw.close();
+                        }
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            disconnect();
+        }
     }
 }
