731e21887eb8b11d17ae4a66707e31ab16bcc59d
Konstantinos Karras <81237959+KostasKarras@users.noreply.github.com>
2021-04-18 22:46:18 +0300

    Update VideoFile.java

--- src/VideoFile.java
+++ src/VideoFile.java
@@ -1,42 +1,55 @@
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 
 public class VideoFile {
 
-    private ArrayList<String> associatedHashtags;
+    /** Class Variables */
     private String filepath;
-    //private ArrayList<String> associatedHashtags;
-    public VideoFile(String filepath, ArrayList<String> associatedHashtags) {
+    private ArrayList<String> associatedHashtags;
+    private int videoID;
+
+    /** Constructor */
+    public VideoFile (String filepath, ArrayList<String> associatedHashtags) {
         this.filepath = filepath;
         this.associatedHashtags = associatedHashtags;
     }
 
     public byte[] getVideoFileChunk() {
         String filepath = this.getFilepath();
+        byte[] buffer = null;
         try {
-            FileInputStream fin = new FileInputStream(new File(filepath));
-            byte[] buffer = new byte[(int)new File(filepath).length()];
-            fin.read(buffer);
-            fin.close();
+            File file = new File(filepath);
+            FileInputStream fis = new FileInputStream(file);
+            buffer = new byte[(int)file.length()];
+            fis.read(buffer);
+            fis.close();
             return buffer;
         }
         catch (IOException e) {
             e.printStackTrace();
         }
-        return new byte[0];
+        return buffer;
     }
 
+    /** Getters */
     public String getFilepath() {
-        return filepath;
+        return this.filepath;
     }
 
     public ArrayList<String> getAssociatedHashtags(){
         return associatedHashtags;
     }
 
+    public int getVideoID() {
+        return videoID;
+    }
+
+    /** Setters */
     public void addAssociatedHashTags(String hashtag){
         associatedHashtags.add(hashtag);
+    }
+
+    public void setVideoID(int videoID) {
+        this.videoID = videoID;
     }
 }
