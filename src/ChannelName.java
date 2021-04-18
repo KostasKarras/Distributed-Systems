516f5b765bd7f4a220cb44f18c8580119bd6da77
Konstantinos Karras <81237959+KostasKarras@users.noreply.github.com>
2021-04-18 14:53:53 +0300

    Update ChannelName.java

--- src/ChannelName.java
+++ src/ChannelName.java
@@ -5,11 +5,83 @@ public class ChannelName {
 
     private String channelName;
     private ArrayList<String> hashtagsPublished;
-    private HashMap<String, ArrayList<Value>> userVideoFilesMap;
+    private HashMap<String, ArrayList<VideoFile>> hashtagVideoFilesMap;
+    private int counterVideoID;
+    private HashMap<Integer, VideoFile> ID_VideoFileMap;
 
-    public ChannelName(String channelName, ArrayList<String> hashtagsPublished, HashMap<String, ArrayList<Value>> userVideoFilesMap){
+    /** Constructors */
+
+    //For new users
+    public ChannelName (String channelName) {
+        this.channelName = channelName;
+        hashtagsPublished = new ArrayList<>();
+        hashtagVideoFilesMap = new HashMap<>();
+        counterVideoID = 0;
+        ID_VideoFileMap = new HashMap<>();
+    }
+
+    //To create existing channels
+    public ChannelName (String channelName, ArrayList<String> hashtagsPublished, HashMap<String, ArrayList<VideoFile>>
+            hashtagVideoFilesMap, int counterVideoID, HashMap<Integer, VideoFile> ID_VideoFileMap) {
         this.channelName = channelName;
         this.hashtagsPublished = hashtagsPublished;
-        this.userVideoFilesMap = userVideoFilesMap;
+        this.hashtagVideoFilesMap = hashtagVideoFilesMap;
+        this.counterVideoID = counterVideoID;
+        this.ID_VideoFileMap = ID_VideoFileMap;
+    }
+
+    public void addVideoFile(VideoFile video) {
+        video.setVideoID(counterVideoID);
+        ID_VideoFileMap.put(counterVideoID, video);
+        counterVideoID++;
+
+        ArrayList<String> hashtags = video.getAssociatedHashtags();
+        for (String hashtag : hashtags) {
+            if (hashtagsPublished.contains(hashtag)) {
+                ArrayList<VideoFile> value = hashtagVideoFilesMap.get(hashtag);
+                value.add(video);
+                hashtagVideoFilesMap.put(hashtag, value);
+            } else {
+                ArrayList<VideoFile> value = new ArrayList<>();
+                value.add(video);
+                hashtagVideoFilesMap.put(hashtag, value);
+
+                this.addHashTag(hashtag);
+            }
+        }
+    }
+
+    public void removeVideoFile(VideoFile video) {
+        ID_VideoFileMap.remove(video.getVideoID());
+
+        ArrayList<String> hashtags = video.getAssociatedHashtags();
+        for (String hashtag : hashtags) {
+            if (hashtagVideoFilesMap.get(hashtag).size() == 1) {
+                hashtagVideoFilesMap.remove(hashtag);
+            } else {
+                ArrayList<VideoFile> value = hashtagVideoFilesMap.get(hashtag);
+                value.remove(video);
+                hashtagVideoFilesMap.put(hashtag, value);
+            }
+        }
+    }
+
+    /** Getters */
+
+    public ArrayList<VideoFile> getVideoFiles_byHashtag(String hashtag) {
+        return hashtagVideoFilesMap.get(hashtag);
+    }
+
+    public VideoFile getVideoFile_byID (int ID) {
+        return ID_VideoFileMap.get(ID);
+    }
+
+    /** Setters */
+    public void addHashTag(String hashtag) {
+        hashtagsPublished.add(hashtag);
+    }
+
+    public void removeHashTag(String hashtag) {
+        hashtagsPublished.remove(hashtag);
     }
 }
