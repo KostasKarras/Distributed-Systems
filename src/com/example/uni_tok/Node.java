86cbafac299f5407c075c2ebbfa4a96041804a13
Konstantinos Karras <81237959+KostasKarras@users.noreply.github.com>
2021-07-11 14:53:27 +0300

    Add files via upload

---
+++ src/com/example/uni_tok/Node.java
@@ -0,0 +1,21 @@
+package com.example.uni_tok;
+
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.List;
+
+interface Node {
+
+    static List<Broker> brokers = new ArrayList<>();
+
+    public void initialize(int port) throws UnknownHostException;
+
+//    public TreeMap<Integer, SocketAddress> getBrokerMap();//DIMITRIS
+
+    public void connect();
+
+    public void disconnect();
+
+//    public void updateNodes() throws IOException;
+
+}
---
+++ src/com/example/uni_tok/VideoInformation.java
@@ -0,0 +1,49 @@
+package com.example.uni_tok;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class VideoInformation implements Serializable, Comparable<VideoInformation> {
+    private ChannelKey ck;
+    private String videoTitle;
+    private ArrayList<String> associatedHashtags;
+    private Date date;
+    private byte [] thumbnail;
+
+    public VideoInformation(ChannelKey ck, String videoTitle, ArrayList associatedHashtags, byte [] thumbnail) {
+        this.ck = ck;
+        this.videoTitle = videoTitle;
+        this.associatedHashtags = associatedHashtags;
+        this.thumbnail = thumbnail;
+        date = ck.getDate();
+    }
+
+    public ChannelKey getChannelKey() {
+        return ck;
+    }
+
+    public int getVideoID(){
+        return ck.getVideoID();
+    }
+
+    public String getChannelName(){
+        return ck.getChannelName();
+    }
+
+    public String getTitle(){ return videoTitle; }
+
+    public ArrayList<String> getHashtags(){
+        return associatedHashtags;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public byte[] getThumbnail() { return this.thumbnail; }
+
+    public int compareTo(VideoInformation vi) {
+        return this.getDate().compareTo(vi.getDate()) * (-1);
+    }
+}
