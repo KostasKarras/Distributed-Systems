4397cfec800569f966fa74f03e0b448c2fa558a7
Konstantinos Karras <81237959+KostasKarras@users.noreply.github.com>
2021-08-24 19:13:25 +0300

    Update Broker.java

--- src/com/example/uni_tok/Broker.java
+++ src/com/example/uni_tok/Broker.java
@@ -1,23 +1,10 @@
 package com.example.uni_tok;
 
 import java.util.HashMap;
-import java.util.List;
 
 interface Broker extends Node {
 
-    static final List<Consumer> registerUsers = null;
-    static final List<Publisher> registerPublishers = null;
-
     public int calculateKeys(String id);
 
-//    public com.example.uni_tok.Publisher acceptConnection(com.example.uni_tok.Publisher publisher);
-//
-//    public com.example.uni_tok.Consumer acceptConnection(com.example.uni_tok.Consumer consumer);
-//
-//    public void notifyPublisher(String str);
-
-//    public void notifyBrokersOnChanges();
-
-    public HashMap<ChannelKey, String> filterConsumers(HashMap<ChannelKey, String> videoList, String channelName);//DEN XRHSIMOPOIEITAI
-
+    public HashMap<ChannelKey, String> filterConsumers(HashMap<ChannelKey, String> videoList, String channelName);
 }
