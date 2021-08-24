5ba034b0e6038c62ebd7cd9aa3d8a8c5f1ebb3f8
Konstantinos Karras <81237959+KostasKarras@users.noreply.github.com>
2021-08-24 19:15:59 +0300

    Update Publisher.java

--- src/com/example/uni_tok/Publisher.java
+++ src/com/example/uni_tok/Publisher.java
@@ -7,19 +7,13 @@ import java.util.ArrayList;
 
 interface Publisher extends Node{
 
-    static final Channel channel = null;
-
     public void addHashTag(VideoFile video);
 
     public void removeHashTag(VideoFile video);
 
-//    public List<com.example.uni_tok.Broker> getBrokerList();
-
     public SocketAddress hashTopic(String hashtopic);
 
     public void push(int id, ObjectInputStream objectInputStream, ObjectOutputStream objectOutputStream);
-
-//    public void notifyFailure(com.example.uni_tok.Broker broker); MALLON THA FYGEI
 
     public void notifyBrokersForHashTags(String hashtag, String action);
 
