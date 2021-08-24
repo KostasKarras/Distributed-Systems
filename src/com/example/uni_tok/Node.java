53cb40b84b4556ec2a91122a46eacb8fb45b9b25
Konstantinos Karras <81237959+KostasKarras@users.noreply.github.com>
2021-08-24 19:15:45 +0300

    Update Node.java

--- src/com/example/uni_tok/Node.java
+++ src/com/example/uni_tok/Node.java
@@ -6,16 +6,9 @@ import java.util.List;
 
 interface Node {
 
-    static List<Broker> brokers = new ArrayList<>();
-
     public void initialize(int port) throws UnknownHostException;
-
-//    public TreeMap<Integer, SocketAddress> getBrokerMap();//DIMITRIS
 
     public void connect();
 
     public void disconnect();
-
-//    public void updateNodes() throws IOException;
-
 }
