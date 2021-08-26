2a493e05b36a4aa50490768a4b5cca85760d59c9
MichaelDik <72825303+MichaelDik@users.noreply.github.com>
2021-08-26 17:43:58 +0300

    Updated Project

--- src/Consumer.java
+++
@@ -1,9 +0,0 @@
-interface Consumer extends Node{
-
-    public void register(Broker broker, String str);
-
-    public void disconnect(Broker broker, String str);//MALLON AXRISTO?
-
-    public void playData(String str, VideoFile video);
-
-}
