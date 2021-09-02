82c81db4ea257ada20bcf949b16552c62e8e320b
Konstantinos Karras <81237959+KostasKarras@users.noreply.github.com>
2021-09-02 23:04:33 +0300

    Update AddressKeeper.java

--- src/com/example/uni_tok/AddressKeeper.java
+++ src/com/example/uni_tok/AddressKeeper.java
@@ -17,16 +17,21 @@ public class AddressKeeper {
     public void init(){
         Socket connectionSocket = null;
         try {
-            serverSocket = new ServerSocket(4000, 60, InetAddress.getByName("0.0.0.0"));
+            //I USE THIS SOCKET TO GET THE IP
+            Socket socket = new Socket(InetAddress.getByName("google.com"), 80);
+            String ip = socket.getLocalAddress().toString();
+            socket.close();
+            //to remove the slash symbol from the ip address I use the substring(1)
+            System.out.println("ADDRESS KEEPER IP: " + ip.substring(1));
+            serverSocket = new ServerSocket(4000, 60, InetAddress.getByName(ip.substring(1)));
 
             /**IMPORTANT
              * If I want to run the app from emulator
-             * I must uncomment the line 27 and
-             * comment the line 20 */
+             * I must uncomment the lines 33-34 and
+             * comment the lines 21-23 and 25-26 */
 
 //            serverSocket = new ServerSocket(4000, 60, InetAddress.getLocalHost());
-
-            System.out.println("Address Keeper IP : " + InetAddress.getLocalHost().getHostAddress());
+//            System.out.println("Address Keeper IP : " + InetAddress.getLocalHost().getHostAddress());
 
             while(true) {
                 connectionSocket = serverSocket.accept();
